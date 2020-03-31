66cf97f7965ed10f0fda057a5b241f7edeab072d
Stephen Studer <stephenkstuder@gmail.com>
2020-03-31 08:54:33 -0700

    got the coloring of div's working

--- script.js
+++ script.js
@@ -9,28 +9,40 @@ $("#currentDay").text("Today's date: " + m.format("dddd MMMM Mo, YYYY"));
 
 //Dynamic creation of hour blocks using jQuery with data-time for time
 const workHours = 9;
-
+const startHour = 9;
+//Time Block Creater
 for (let i = 0; i < workHours; i++){
-    $(`<div id="append-time-block" data-time="${i+workHours}" class="container d-flex flex-column justify-content-center align-items-center">
+    $(`<div data-time="${i+startHour}" class="append-time-block container d-flex flex-column justify-content-center align-items-center">
     <div class="time-block row d-flex">
     <div class="hour d-flex align-items-center">
-    <h3>${convertToTime(i+workHours)}</h3>
+    <h3>${convertToTime(i+startHour)}</h3>
     </div>
     <textarea class="description"></textarea>
     <img class="saveBtn button" src="./images/save.png" width="80">
     </div>
     </div>`).appendTo($("#planner"));
-    determineDivColor();
+    //Logic that colors blocked based on time TODO
 }
-//Logic that colors blocked based on time TODO
-function determineDivColor(){
-    if($("#append-time-block").data("time").text <  m.format("hh")){
 
-        $(".time-block").addClass("past");
-    }
-    // $(".time-block").addClass("current");
-    // $(".time-block").addClass("future");
-}
+$(".append-time-block").each(function(){
+    var testdata = $(this).attr('data-time');
+    console.log(testdata);
+      if (testdata - Number(m.format("H")) === 0) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        } else if (testdata - Number(m.format("H")) < 0){
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        } else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+                
+      }
+});
+
 //time conversion for displaying hour of the day
 function convertToTime(number){
     if (number < 12){
