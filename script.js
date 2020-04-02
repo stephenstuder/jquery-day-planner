18abf1142076bb1c04d0748119fbd74a49176672
Stephen Studer <stephenkstuder@gmail.com>
2020-04-01 21:26:53 -0700

    Began save and retrieve with local storage

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 //Displaying and formatting dates and times
 const m = moment();
 
-console.log(m.toString());
-console.log(m.format("hh"));
-
 //Display the current day
 $("#currentDay").text("Today's date: " + m.format("dddd MMMM DD, YYYY"));
 
 //Dynamic creation of hour blocks using jQuery with data-time for time
 const workHours = 9;
 const startHour = 9;
+
 //Time Block Creater
 for (let i = 0; i < workHours; i++){
     $(`<div data-time="${i+startHour}" class="append-time-block container d-flex flex-column justify-content-center align-items-center">
@@ -27,8 +25,7 @@ for (let i = 0; i < workHours; i++){
 //Loop over divs and color backgrounds
 $(".append-time-block").each(function(){
     var testdata = $(this).attr('data-time');
-    console.log(testdata);
-      if (testdata - Number(m.format("H")) === 0) {
+          if (testdata - Number(m.format("H")) === 0) {
             $(this).children("div:first").removeClass("past");
             $(this).children("div:first").removeClass("future");
             $(this).children("div:first").addClass("present");
@@ -54,5 +51,48 @@ function convertToTime(number){
         number = `${number-12}PM`;
     } return number;
 }
+let plannerText = [];
+plannerText.length = workHours + startHour;
+
+init();
+
+$(".saveBtn").on("click", function(){
+    let textBox = $(this).prev("textarea");
+    let textValue = textBox[0].value;
+    
+    let timeValue = textBox.parent("div").parent("div").attr("data-time");
+   
+    plannerText[timeValue] = textValue;
+    console.log(plannerText);
+    saveToLocal();
+})
+
+function saveToLocal(){
+    localStorage.setItem(JSON.stringify("plannerStorage"), plannerText);
+}
+
+function fillFromLocal(){
+    let storedLeaderboard = JSON.parse(localStorage.getItem("plannerStorage"));
+    console.log(storedLeaderboard);
+    $(".saveBtn").each(function(){
+        let textBox = $(this).prev("textarea");
+        let textValue = textBox[0].value;
+        let timeValue = textBox.parent("div").parent("div").attr("data-time");
+        $.each(storedLeaderboard, function(key, valueObj){
+            if (timeValue == storedLeaderboard.indexOf[key]){
+                //textBox.append(valueObj);
+            }
+        })
+    });
+}
+fillFromLocal();
+
+function init(){
+    let oldItems = JSON.parse(localStorage.getItem("plannerStorage"));
+    plannerText = oldItems;
+}
 
 //Button listener that saves content to local storage
+//Add value pair to local storage
+//Retrieve on each page load and place where appropriate 
+//Clear planner button with an are you sure popup. 
